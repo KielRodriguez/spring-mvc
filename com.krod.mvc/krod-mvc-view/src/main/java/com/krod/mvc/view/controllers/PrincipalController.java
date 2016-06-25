bd8afaf29f9d88c841e3e1d3f66b2d51f4c57cc3
krodriguez <kr.rdz.20@gmail.com>
2016-06-25 00:29:02 -0500

    Configuracion de poms en distintos proyectos, configuracion web de metodos

--- com.krod.mvc/krod-mvc-view/src/main/java/com/krod/mvc/view/controllers/PrincipalController.java
+++ com.krod.mvc/krod-mvc-view/src/main/java/com/krod/mvc/view/controllers/PrincipalController.java
@@ -1,6 +1,14 @@
 package com.krod.mvc.view.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/application")
 public class PrincipalController {
-
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
 }
